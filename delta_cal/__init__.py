bafa224bf8ba7a8e41e4f570beaaa193688e99c3
th0mpy <cs.thompson@mac.com>
2017-06-14 09:52:29 -0500

    updates

--- delta_cal/__init__.py
+++ delta_cal/__init__.py
@@ -1,14 +1,18 @@
 #coding=utf-8
 from __future__ import absolute_import
 
+
+
+
+
+
+
+
 import octoprint.plugin
 import octoprint.server
 
 class DeltaCalPlugin(octoprint.plugin.AssetPlugin,
                             octoprint.plugin.TemplatePlugin):
-    def on_after_startup(self):
-        self._logger.info("Started up!")
-		
     def get_assets(self):
         return dict(
             js=["js/deltaautocal.js"]
