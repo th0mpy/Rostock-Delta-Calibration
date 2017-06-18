ddb375fa5e8d9b35d4839baa700101bf1eb2cde8
th0mpy <cs.thompson@mac.com>
2017-06-18 10:03:55 -0500

    Remove sleep

--- delta_cal/static/js/deltaautocal.js
+++ delta_cal/static/js/deltaautocal.js
@@ -1,16 +1,5 @@
 $(function () {
-    "use strict";
-	function sleep(milliseconds)
-	{
-		var start = new Date().getTime();
-		for (var i = 0; i < 1e7; i++)
-		{
-			if ((new Date().getTime() - start) > milliseconds)
-			{
-				break;
-			}
-		}
-	}		
+    "use strict";		
     function DeltaAutoCalViewModel(parameters) {
         var self = this;
         self.control = parameters[0];
